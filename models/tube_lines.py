fd3b90dc96ee353b0925302bb9f98b27c074c935
Harry Watkinson <harryw@peachmedia.com>
2024-08-29 20:10:26 +0100

    Add database

--- models/tube_lines.py
+++ models/tube_lines.py
@@ -3,5 +3,5 @@ from neomodel import StringProperty
 
 
 class Piccadilly(Station):
-    line_name = StringProperty(default="Piccadilly")
+    line_name = StringProperty(default="Piccadilly", required=True)
     line_colour = StringProperty(default="#1C1865", required=True)
