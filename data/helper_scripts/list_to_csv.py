8b30b2a054a2de972b9d8cae18f31f50f33d752f
Harry Watkinson <harryw@peachmedia.com>
2024-08-31 19:37:41 +0100

    Run black

--- data/helper_scripts/list_to_csv.py
+++ data/helper_scripts/list_to_csv.py
@@ -3,10 +3,10 @@ from uuid import uuid4
 
 from project_root import get_project_root
 
-LINE_CODE = 'P'
+LINE_CODE = "P"
+
 
 def list_to_stations() -> None:
-
 
     with open(f"{get_project_root()}/data/helper_scripts/clipboard.txt") as file_handle:
         file_contents_list = file_handle.read().split("\n")
@@ -17,7 +17,9 @@ def list_to_stations() -> None:
         if "," in item:
             item = f'"{item}"'
         # station_name,end_of_line,tube_line_identifier,station_identifier,location,year_opened,wiggle_ranking
-        print(f'{item},False,{LINE_CODE}{index},{int(sha256(item.encode("utf-8")).hexdigest(), 16) % (10 ** 8)},"[lon, lat]",,0.0')
+        print(
+            f'{item},False,{LINE_CODE}{index},{int(sha256(item.encode("utf-8")).hexdigest(), 16) % (10 ** 8)},"[lon, lat]",,0.0'
+        )
 
 
 def list_to_connections() -> None:
@@ -37,11 +39,15 @@ def list_to_connections() -> None:
                 f"{item},{LINE_CODE}{index},{LINE_CODE}{len(file_contents_list[:-2])},False,0,0,{uuid4()}"
             )
         else:
-            print(f"{item},{LINE_CODE}{index},{LINE_CODE}{index - 1},False,0,0,{uuid4()}")
-            print(f"{item},{LINE_CODE}{index},{LINE_CODE}{index + 1},True,0,0,{uuid4()}")
+            print(
+                f"{item},{LINE_CODE}{index},{LINE_CODE}{index - 1},False,0,0,{uuid4()}"
+            )
+            print(
+                f"{item},{LINE_CODE}{index},{LINE_CODE}{index + 1},True,0,0,{uuid4()}"
+            )
 
 
 if __name__ == "__main__":
     list_to_stations()
-    print('\n\n\n')
+    print("\n\n\n")
     list_to_connections()
